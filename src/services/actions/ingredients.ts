5f568f2b018cabe8b7dda1ae173b384d4575d885
YanPrika <yan_prika@mail.ru>
2023-11-15 20:03:26 +0300

    Updates

--- src/services/actions/ingredients.ts
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import {getIngredients as getIngr} from "../../utils/api"
-
-export const getIngredients = createAsyncThunk(
-    "getIngredients",
-    getIngr
-);
--- src/services/actions/orders.ts
+++
@@ -1,7 +0,0 @@
-import { createOrder as createOrderApi } from "../../utils/api";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
-export const createOrder = createAsyncThunk(
-  "order/createOrder",
-  createOrderApi
-);
